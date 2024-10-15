faa8abaaf102d7549e3e7da6f7e59f90c7961186
Matheus Marques <matheusmarujo26@gmail.com>
2024-10-15 14:48:13 -0300

    Update main.py
    Atualizando sem a útilização do mouse

--- main.py
+++ main.py
@@ -1,222 +1,171 @@
+from selenium import webdriver
 from turtle import position
 import pyautogui
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import time
+import os
+import shutil
+import win32com.client
 
-def limpar_Downloads():
-    ''' Entra em Downloads e excluí todos os arquivos '''
-    pyautogui.press('winleft')
-    pyautogui.write('Download')
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.hotkey('winleft','up')
-    pyautogui.hotkey('ctrl','a')
-    pyautogui.press('delete')
-    pyautogui.hotkey('ctrl','w')
+def exportar():
+    time.sleep(30)
+    try:
+        
+        export_button = WebDriverWait(navegador, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="ZDBExportButton"]'))
+        )
+        export_button.click()
 
-def click_Mouse_Padrao_exportar():
-    ''' Aperta nos botões para exportar um relatório do Nomus '''
-    time.sleep(25)
-    pyautogui.press('esc')
-    pyautogui.moveTo(1643,109)
-    pyautogui.click()
-    pyautogui.moveTo(1664,166)
-    pyautogui.click()
-    pyautogui.moveTo(1458,1012)
-    pyautogui.click()
+        embed_export_button = WebDriverWait(navegador, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="EmbedExportXLSMenuItem"]'))
+        )
+        time.sleep(1)
+        embed_export_button.click()
+        print("Botão 'EmbedExportXLSMenuItem' clicado com sucesso!")
+        
+        final_button = WebDriverWait(navegador, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '/html/body/div[32]/div/div[2]/div/footer/div/button[1]'))
+        )
+        time.sleep(1)
+        final_button.click()
 
-def click_Mouse_Padrao_exportar_Pausas():
-    ''' Aperta nos botões para exportar um relatório do Nomus com pausas de 5 segundos entre cada click '''
-    time.sleep(25)
-    pyautogui.press('esc')
-    pyautogui.moveTo(1643,109)
-    pyautogui.click()
-    time.sleep(5)
-    pyautogui.moveTo(1664,166)
-    pyautogui.click()
-    time.sleep(5)
-    pyautogui.moveTo(1458,1012)
-    pyautogui.click()
-    time.sleep(5)
+        time.sleep(1)
 
-def click_Mouse_Diferente_exportar():
-    ''' Aperta nos botões para exportar um relatório do Nomus com a visualização diferente '''
-    pyautogui.hotkey('ctrl','shift','tab')
-    time.sleep(15)
-    pyautogui.press('esc')
-    pyautogui.moveTo(1761,111)
-    pyautogui.click()
-    pyautogui.moveTo(1696,164)
-    pyautogui.click()
-    pyautogui.moveTo(1458,1012)
-    pyautogui.click()
-    pyautogui.hotkey('ctrl','t')
-    
-def entrar_Link(texto):
-    ''' Entra no link '''
-    pyautogui.write(texto)
-    pyautogui.press('enter')
-    pyautogui.hotkey('ctrl','t')
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
 
-def abrir_Navegador(texto):
-    ''' Abre o navegador '''
-    time.sleep(5)
-    pyautogui.press('winleft')
-    pyautogui.write(texto)
-    pyautogui.press('enter')
-    pyautogui.hotkey('winleft','up')
-    pyautogui.moveTo(839,62)
-    pyautogui.click()
+def mudar_Link(texto,i):
+    navegador.execute_script("window.open('');")
+    navegador.switch_to.window(navegador.window_handles[i])
+    navegador.get(texto)
 
-def exportar_relatorio_padrao():
-    ''' Baixa os arquivos '''
-    pyautogui.hotkey('ctrl','tab')
-    click_Mouse_Padrao_exportar()
-
-def exportar_relatorio_padrao_Pausas():
-    ''' Baixa os arquivos com pausas de 5 segundos por click'''
-    pyautogui.hotkey('ctrl','tab')
-    click_Mouse_Padrao_exportar_Pausas()
-    
-def exportar_Relatorio_Diferente():
-    ''' Baixa os arquivos com os relatórios com visualizações diferentes'''
-    click_Mouse_Padrao_exportar()
-    pyautogui.hotkey('ctrl','shift','tab')
-
-def exportar_Fechar_Navegador_Padrao():
-    '''Conjunto de funções para exportar todos os arquivos e fechar o navegador ao terminar'''
-    time.sleep(1)
-    pyautogui.hotkey('ctrl','w')
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    exportar_relatorio_padrao()
-    time.sleep(60)
-    pyautogui.hotkey('ctrl','shift','w')
-
-def exportar_Fechar_Navegador_Padrao_Pausas():
-    '''Conjunto de funções para exportar todos os arquivos e fechar o navegador ao terminar com pausas de 5 segundos por click'''
-    time.sleep(1)
-    pyautogui.hotkey('ctrl','w')
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    exportar_relatorio_padrao_Pausas()
-    time.sleep(60)
-    pyautogui.hotkey('ctrl','shift','w')
-
-def exportar_Fechar_Navegador_Diferente():
-    '''Conjunto de funções para exportar todos os arquivos com relatorios de visualizações diferentes e fechar o navegador ao terminar'''
-    pyautogui.hotkey('ctrl','w')
-    exportar_Relatorio_Diferente()
-    exportar_Relatorio_Diferente()
-    exportar_Relatorio_Diferente()
-    time.sleep(60)
-    pyautogui.hotkey('ctrl','shift','w')
-
-def atualizar_Novos_Arquivos():
-    ''' Recorta os arquivos em Download e cola no servidor '''
-    pyautogui.press('winleft')
-    pyautogui.write('Download')
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.hotkey('winleft','up')
-    pyautogui.hotkey('ctrl','a')
-    pyautogui.hotkey('ctrl','x')
-    pyautogui.hotkey('ctrl','w')
-    pyautogui.press('winleft')
-    pyautogui.write('Z:\ISO 9000 - SGQ')
-    pyautogui.press('enter')
-    time.sleep(1)
-    pyautogui.press('pgdn')
-    pyautogui.press('enter')
-    pyautogui.press('pgup')
-    pyautogui.press('enter')    
-    pyautogui.hotkey('winleft','up')
-    pyautogui.hotkey('ctrl','v')
+def exporta_Nomus():
+    navegador.get('https://reports.nomus.com.br/open-view/751489003498986462') # PC - Follow up
     time.sleep(10)
-    pyautogui.press('enter')
-    time.sleep(1)
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003495455916',1) # SC COMPLETA
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003499869977',2) # Cotação
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003499311067',3) # NF Tipo de movimentação
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003497642590',4) # Fornecedores - TB-06
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003385051213',5) # Ordens Lead
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003418257074',6) # Necessidade
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003348744953',7) # ID-04e
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003473916593',8) # Tempo de produção
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003396077915',9) # Entradas
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003343390887',10) # Pedido de compra
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003375648746',11) # Pv's
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003343401232',12) # SC's
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003343402198',13) # Entradas compras
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003383877015',14) # Faturamento periodo
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003384381506',15) # CRM
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003381705320',16) # Vendas
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003396836362',17) # Emissão de PV
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003384833393',18) # Status do PV
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003501209517',19) # Familia
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003299922759',20) # CONTROLE DA PRODUÇÃO
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003399788047',21) # ENTRADAS (LIVRO)
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003415814176',22) # Nfs emitidas
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003399436517',23) # Saída 
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003406446494',24) # Dw Compras
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003342815143',25) # Modelo NF's 
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003415906765',26) # Grupos
+    exportar()
+    mudar_Link('https://reports.nomus.com.br/open-view/751489003432511966',27) # movimentação - pvs
+    exportar()
 
-pyautogui.alert("""O código vai começar.
+    time.sleep(60)
+    navegador.quit()
 
-Favor deixar o Capsloock desativado.""")
+def move_Arquivos():
+    for arquivo in os.listdir(pasta_destino):
+        caminho_arquivo = os.path.join(pasta_destino, arquivo)
+        if os.path.isfile(caminho_arquivo):
+            os.remove(caminho_arquivo)
 
-pyautogui.PAUSE = 0.5
+    for arquivo in os.listdir(pasta_downloads):
+        caminho_arquivo = os.path.join(pasta_downloads, arquivo)
+        if os.path.isfile(caminho_arquivo):
+            shutil.move(caminho_arquivo, pasta_destino)
 
-limpar_Downloads()
+    print('Arquivos deletados e novos arquivos movidos com sucesso!')    
 
-abrir_Navegador('chrome')
+def titulo():
+    pyautogui.alert("""O código vai começar.
 
-entrar_Link('https://reports.nomus.com.br/open-view/751489003498986462') # PC - Follow up
-entrar_Link('https://reports.nomus.com.br/open-view/751489003495455916') # SC COMPLETA
-entrar_Link('https://reports.nomus.com.br/open-view/751489003499869977') # Cotação
-entrar_Link('https://reports.nomus.com.br/open-view/751489003499311067') # NF Tipo de movimentação
-entrar_Link('https://reports.nomus.com.br/open-view/751489003497642590') # Fornecedores - TB-06
-entrar_Link('https://reports.nomus.com.br/open-view/751489003385051213') # Ordens Lead
-entrar_Link('https://reports.nomus.com.br/open-view/751489003418257074') # Necessidade
-entrar_Link('https://reports.nomus.com.br/open-view/751489003348744953') # ID-04e
-entrar_Link('https://reports.nomus.com.br/open-view/751489003473916593') # Tempo de produção
-entrar_Link('https://reports.nomus.com.br/open-view/751489003396077915') # Entradas
-exportar_Fechar_Navegador_Padrao()
+Favor não útilizar o PC até a finalização.""")
 
-time.sleep(3)
+def finalizacao():
+    pyautogui.alert("""O código foi finalizado.
 
-abrir_Navegador('brave')
+Não esqueça de pressionar o botão de atualizar nas suas planilhas.
 
-entrar_Link('https://reports.nomus.com.br/open-view/751489003343390887') # Pedido de compra
-entrar_Link('https://reports.nomus.com.br/open-view/751489003375648746') # Pv's
-entrar_Link('https://reports.nomus.com.br/open-view/751489003343401232') # SC's
-entrar_Link('https://reports.nomus.com.br/open-view/751489003343402198') # Entradas compras
-entrar_Link('https://reports.nomus.com.br/open-view/751489003383877015') # Faturamento periodo
-entrar_Link('https://reports.nomus.com.br/open-view/751489003384381506') # CRM
-entrar_Link('https://reports.nomus.com.br/open-view/751489003381705320') # Vendas
-entrar_Link('https://reports.nomus.com.br/open-view/751489003396836362') # Emissão de PV
-entrar_Link('https://reports.nomus.com.br/open-view/751489003384833393') # Status do PV
-entrar_Link('https://reports.nomus.com.br/open-view/751489003501209517') # Familia
-exportar_Fechar_Navegador_Padrao_Pausas()
+Obrigado por aguardar. """)
 
-time.sleep(3)
+def atualizar_Planilhas(local_Planilha):
+    excel = win32com.client.DispatchEx('Excel.Application')
+    excel.Visible = True
 
-abrir_Navegador('firefox')
+    try:
+        wb = excel.Workbooks.Open(local_Planilha)
+        wb.RefreshAll()
+        excel.CalculateUntilAsyncQueriesDone()
+        wb.Close(SaveChanges=True)
+        
+    except Exception as e:
+        print(f"Erro ao abrir ou atualizar a planilha: {e}")
 
-entrar_Link('https://reports.nomus.com.br/open-view/751489003299922759') # CONTROLE DA PRODUÇÃO
-click_Mouse_Diferente_exportar()
-entrar_Link('https://reports.nomus.com.br/open-view/751489003399788047') # ENTRADAS (LIVRO)
-click_Mouse_Diferente_exportar()
-entrar_Link('https://reports.nomus.com.br/open-view/751489003415814176') # Nfs emitidas
-click_Mouse_Diferente_exportar()
-entrar_Link('https://reports.nomus.com.br/open-view/751489003399436517') # Saída 
-click_Mouse_Diferente_exportar()
-pyautogui.hotkey('ctrl','tab')
-pyautogui.hotkey('ctrl','tab')
-pyautogui.hotkey('ctrl','w')
-pyautogui.hotkey('ctrl','tab')
-pyautogui.hotkey('ctrl','w')
-pyautogui.hotkey('ctrl','tab')
-pyautogui.hotkey('ctrl','w')
-pyautogui.hotkey('ctrl','tab')
-pyautogui.hotkey('ctrl','w')
-entrar_Link('https://reports.nomus.com.br/open-view/751489003406446494') # Dw Compras
-entrar_Link('https://reports.nomus.com.br/open-view/751489003342815143') # Modelo NF's 
-entrar_Link('https://reports.nomus.com.br/open-view/751489003415906765') # Grupos
-exportar_Fechar_Navegador_Diferente()
+    finally:
+        excel.Quit()
 
-time.sleep(3)
+def atualizar_Excel():
+    atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\9 - PPCP\\Lead time\\Banco de dados\\Banco de dados - Lead.xlsx')
+    atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\9 - PPCP\\Lead time\\Lead time - Familia.xlsx')
+    atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\9 - PPCP\\.PCP\\Fechamento de estoque\\Monitoramento de estoque.xlsx')
+    # atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\9 - PPCP\\.PCP\\Controle\\Controle.xlsx')
+    # atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\5-PROCESSO PROJETOS\\REGISTROS\\Controle - Ass.xlsx')
+    # atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\3-GESTÃO DA QUALIDADE\\REGISTROS\\Gráficos-IDs-PCP-2024 - IDs 04a 04b 13b.xlsx')
+    # atualizar_Planilhas('Z:\\ISO 9000 - SGQ\\3-GESTÃO DA QUALIDADE\\REGISTROS\\Graficos-IDs-Compras-2024 - IDs 09 13a 13c.xlsx')
 
-atualizar_Novos_Arquivos()
+titulo()
 
-pyautogui.alert("""Código finalizado.
+pasta_downloads = 'c:\\Users\\Tecnico4\\Downloads'
+pasta_destino = 'Z:\\ISO 9000 - SGQ\\12-SISTEMA\\0. Banco de dados - Planilhas'
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
 
-Obrigado por aguardar.""")
+exporta_Nomus()
+
+move_Arquivos()
+
+atualizar_Excel()
+
+finalizacao()
